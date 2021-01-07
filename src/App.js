6d9a5220620f259878c29c2118e664d95f4f1fe6
Sultan Karabekov <sulik23@gmail.com>
2021-01-06 19:00:06 -0500

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
 import { Route, Switch } from 'react-router-dom ';
-
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import logo from '../images/logo.svg';
+import { FaAlignRight } from 'react-icons/fa'
+
+
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Navbar
+            </div>
+        )
+    }
+}
