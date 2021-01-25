2b342f4f3a3735726c388149000c099100c81731
Sultan Karabekov <sulik23@gmail.com>
2021-01-24 19:31:49 -0500

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
-import { Route, Switch } from 'react-router-dom ';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link } from 'react-router-dom ';
 import Navbar from "./components/Navbar"
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import logo from '../images/logo.svg';
-import { FaAlignRight } from 'react-icons/fa'
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 
 
 
 export default class Navbar extends Component {
+    state={
+        isOpen:false
+    }
+    handleToggle = () => {
+        this.setState({isOpen: !this.state.isOpen})
+    }
     render() {
-        return (
-            <div>
-                Navbar
-            </div>
-        )
+        return 
+        <nav className="navbar">
+        <div className="nav-center">
+        <div className="nav-header">
+        <Link to="/">
+        <img src={logo} alt="Beach Resort" />
+        </Link>
+        </div>
+        </div>
+        </nav>
     }
 }
