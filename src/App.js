8f759586f687e8fa98563f53f3b6d69e29dacfb7
Sultan Karabekov <sulik23@gmail.com>
2021-01-27 13:56:52 -0500

    latest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
-import Home from './pages/Home'
-import Rooms from './pages/Rooms'
-import SingleRoom from './pages/SingleRoom'
-import Error from './pages/Error'
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
 
 import {
   Switch,
