9ff567d2937d94c7d2a452b3d6008311b8182fd1
Sultan Karabekov <sulik23@gmail.com>
2021-01-26 22:58:34 -0500

    latest

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
-
 const home = () => {
   return (
     <>
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -3,8 +3,6 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import RoomsContainer from "../components/RoomsContainer";
-
-
 const Rooms = () => {
   return (
     <>
